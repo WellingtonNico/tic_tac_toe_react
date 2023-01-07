ac1aa2a671a3ba4d94bda7b5f53da4ef50c3b4be
Wellington Adriano Nicoletti de Abreu <wellingtonicoletti@gmail.com>
2023-01-07 10:25:43 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,119 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const initialPositions = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  const [positions, setPositions] = useState(initialPositions)
+  const [player, setPlayer] = useState(-1)
+  const [winningIndexes, setWinningWindexes] = useState(null)
+  const [hasNoWinner, setHasNoWinner] = useState(false)
+  const [winnerPlayer, setWinnerPlayser] = useState(null)
+
+  const winnningPositions = [
+    // horizontal
+    { indexes: [0, 1, 2], orientation: 'horizontal' },
+    { indexes: [3, 4, 5], orientation: 'horizontal' },
+    { indexes: [6, 7, 8], orientation: 'horizontal' },
+
+    // vertical
+    { indexes: [0, 3, 6], orientation: 'vertical' },
+    { indexes: [1, 4, 7], orientation: 'vertical' },
+    { indexes: [2, 5, 8], orientation: 'vertical' },
+
+    // diagonal
+    { indexes: [0, 4, 8], orientation: 'diagonal1' },
+    { indexes: [2, 4, 6], orientation: 'diagonal2' },
+  ]
+
+  const handleClicked = (clickedIndex) => {
+    if (positions[clickedIndex] != 0 || winnerPlayer != null) {
+      return
+    }
+    setPositions(positions.map((position, index) => {
+      if (index == clickedIndex) {
+        return player
+      }
+      return position
+    }))
+  }
+
+  const resetGame = () => {
+    setPositions(initialPositions)
+    setWinningWindexes(null)
+    setPlayer(-1)
+    setWinnerPlayser(null)
+    setHasNoWinner(false)
+  }
+
+  const sumArray = (array) => {
+    return array.reduce((accummulator, value) => accummulator + value, 0)
+  }
+
+  const checkWinner = () => {
+    winnningPositions.forEach(position => {
+      const currentCheckinPositions = positions.filter(
+        (p, index) => position.indexes.includes(index)
+      )
+      if ([-3, 3].includes(sumArray(currentCheckinPositions))) {
+        setWinningWindexes(position)
+        setWinnerPlayser(player)
+      }
+    })
+    setPlayer(player == 1 ? -1 : 1)
+  }
+
+  const checkBrokenGame = () => {
+    if (winnerPlayer == null && positions.every(value => value != 0)) {
+      setHasNoWinner(true)
+    }
+  }
+
+  useEffect(() => {
+    checkWinner()
+  }, [positions])
+
+  useEffect(() => {
+    checkBrokenGame()
+  }, [player])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {winnerPlayer != null &&
+        <div >
+          <span className='winner'>Parabens ao Jogador </span>
+          <span className='winner-icon'>{winnerPlayer == 1 ? '❌' : '⭕️'}</span>
+        </div>
+      }
+      {hasNoWinner &&
+        <div className="broken-game">
+          <span>Deu Velha</span>
+        </div>
+      }
+      <div className='blocks'>
+        {positions.map((position, index) =>
+          <div
+            className={
+              `block ${winningIndexes?.indexes.includes(index) ?
+                `animated-winner ${winningIndexes.orientation}` :
+                ''
+              }`
+            }
+            key={index}
+            onClick={() => handleClicked(index)}
+          >
+            <span>
+              {position == 1 && 'X'}
+              {position == -1 && 'O'}
+            </span>
+          </div>
+        )}
+
+      </div>
+      <div>
+        {(winnerPlayer != null || hasNoWinner == true) &&
+          <button onClick={resetGame} className='restart-button'>Reiniciar</button>
+        }
+      </div>
     </div>
   );
 }
